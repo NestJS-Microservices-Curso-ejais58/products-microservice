dbabd301efad2f73a0964ff6ca906571fe127dcb
Eliasjais <ejais58@gmail.com>
2024-09-01 17:51:00 -0300

    update product service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -50,7 +51,12 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       })
   
       if (!product){
-        throw new NotFoundException(`Product with id ${id} not found`);
+        throw new RpcException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            message:`Product with id ${id} not found`
+          }
+      );
       }
 
       return product
